458b4cc361f333d7a5bae12b20668d8fce675bdc
Igor Sedura <igorsedura@gmail.com>
2023-04-26 08:58:06 +0300

    bugfix

--- index.js
+++ index.js
@@ -23,16 +23,22 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await listContacts();
-      return console.log(allContacts);
+      console.table(allContacts);
+      break;
     case "get":
       const oneContact = await getContactById(id);
-      return console.log(oneContact);
+      console.log(oneContact);
+      break;
     case "add":
       const newContact = await addContact({ name, email, phone });
-      return console.log(newContact);
+      console.log(newContact);
+      break;
     case "remove":
       const deleteContact = await removeContact(id);
-      return console.log(deleteContact);
+      console.log(deleteContact);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
   }
 };
 
